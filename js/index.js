42056d87d925d6f8e3bac9d2006f3f5ec698fa64
Vinicius Pereira <agavi2014@hotmail.com>
2020-11-24 00:22:37 -0300

    [NEW] Adicionando produtos no localStorage e exibindo de forma dinamica sem refresh

--- js/index.js
+++ js/index.js
@@ -8,11 +8,18 @@ save_product.addEventListener("click", function(){
     //verificar se localstorage existe
     if(localStorage.getItem("products") != null)
     {
-
+        var deserialize_products = JSON.parse(localStorage.getItem("products"))
+        var new_product = {
+            "product_name": product_name,
+            "product_brand":product_brand,
+            "product_price":product_price
+        }
+        deserialize_products.push(new_product)
+        localStorage.setItem("products", JSON.stringify(deserialize_products))
     }
     else
     {
-        var inicial_cart = [
+        var initial_product = [
             {
                 "product_name": product_name,
                 "product_brand":product_brand,
@@ -20,13 +27,36 @@ save_product.addEventListener("click", function(){
             }
         ]
 
-        localStorage.setItem("products", JSON.stringify(inicial_cart))
+        localStorage.setItem("products", JSON.stringify(initial_product))
     }
 
+    listProducts()
 
 })
 
 //Listar produtos na tabela
 function listProducts(){
+    var tbody = document.getElementById("body_table")
+    var deserialized_local_storage = JSON.parse(localStorage.getItem("products"))
+    //create <tr></tr>
+    $("#body_table").empty()
+    deserialized_local_storage.forEach(obj => {
+        var tr = document.createElement("tr")
+        
+        //create <th></th>
+        var td_name = document.createElement("td")
+        td_name.appendChild(document.createTextNode(obj.product_name))
+
+        var td_brand = document.createElement("td")
+        td_brand.appendChild(document.createTextNode(obj.product_brand))
+
+        var td_price = document.createElement("td")
+        td_price.appendChild(document.createTextNode(obj.product_price))
+
+        tr.appendChild(td_name)
+        tr.appendChild(td_brand)
+        tr.appendChild(td_price)
+        tbody.appendChild(tr)
+    })
 
 }
